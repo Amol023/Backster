5b4ff254974623e36da01710501453482205bd90
msc0030 <msc0030@tigermail.auburn.edu>
2015-11-17 19:45:54 -0600

    Update back-compressive-engine.js
    Line40: changed "===" to "==" inside the if statement.

--- Backster/client/lib/back-compressive-engine.js
+++ Backster/client/lib/back-compressive-engine.js
@@ -37,7 +37,7 @@ var _calculateBMLA = function( userInput ) {
 var _calculateIVDA = function( userInput ) {
   var result = -16.959 + 0.179 * userInput.height * 2.54;
 
-  if (userInput.gender === 'Male') {
+  if (userInput.gender == 'Male') {
     result += 1.7;
   }
 
